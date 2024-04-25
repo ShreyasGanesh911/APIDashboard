20aaa70d8f9cf6c8135fa4aa6b809f291f764e13
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-25 16:56:21 +0530

    react-toastify

---
+++ src/Toast.ts
@@ -0,0 +1,42 @@
+import { toast ,Bounce} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+export  const toastSuccess = (message:String)=>{
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+        });
+}
+
+export const toastError = (message:String)=>{
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+        });
+}
+export const toastWarning = (message:String)=>{
+    toast.warn(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+        });
+}
