b2ada7dccb9b7e4d403b941613c6c9e7ce840760
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-25 23:30:10 +0530

    Added loading modal

---
+++ src/components/Loading/LoadingTable.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LoadingTable() {
+  return (
+    <>
+       
+      <div className={`  h-56 w-11/12 bg-neutral-900  flex justify-center items-center my-5  flex-col  py-12 animate-pulse dark:bg-gray-700`}>
+
+      </div>
+    </>
+  )
+}
