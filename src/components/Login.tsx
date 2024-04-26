9ddf866eb30b49c60e4a19470e5d6e686260b12b
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-26 17:37:54 +0530

    final css added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -58,11 +58,11 @@ export default function Login({form,setForm}:Props) {
   return (
     <>
       <form className=' px-14  bg-neutral-900 rounded-2xl w-3/4 h-4/5  flex flex-col justify-center' onSubmit={handleSubmit}>
-              <h1 className=' text-4xl overflow-hidden font-semibold my-3'>Welcome back !</h1>
+              <h1 className=' text-4xl overflow-hidden font-semibold my-3 overflow-y-hidden'>Welcome back !</h1>
               <p className='py-3'>Don't have an account!? <span className='text-red-500 underline'>< button className='text-lg underline' onClick={()=>{setForm(false)}}>Create Account</button></span></p>
               <div className='flex justify-center items-start flex-col'>
                <div >
-               <label htmlFor="email" className=' text-white tracking-wide text-lg ' >Email:</label>
+               <label htmlFor="email" className=' text-white tracking-wide text-lg overflow-y-hidden' >Email:</label>
                </div>
                 <div className='w-8/12'>
                 <input className='border border-slate-400 w-full p-3 my-3 rounded tracking-wide outline-slate-400 text-black' placeholder='Your Email Address' type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
@@ -70,9 +70,9 @@ export default function Login({form,setForm}:Props) {
               </div>
               <div className='flex justify-center items-start flex-col mt-3'>
                <div>
-               <label htmlFor="password" className='  tracking-wide text-lg text-white ' >Password:</label>
+               <label htmlFor="password" className='  tracking-wide text-lg text-white overflow-y-hidden' >Password:</label>
                </div>
-                <div className='w-3/4 flex items-center'>
+                <div className='w-3/4 flex items-center overflow-y-hidden'>
                 <input className='border border-slate-400 w-full p-3 my-3 rounded outline-slate-400 tracking-wide text-black ' placeholder='Enter Password' type={show} value={password} onChange={(e)=>setPassword(e.target.value)} />
                 <button className="border border-slate-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label relative right-14" onClick={handlePasswordVisibility} >{visibility}</button>
                 </div>
@@ -80,7 +80,7 @@ export default function Login({form,setForm}:Props) {
               <div className='flex justify-center items-start flex-col mt-3 p-10'>
             <p className='invisible'> q</p>
               </div>
-              <div className='flex flex-row-reverse w-full my-3 px-3'>
+              <div className='flex flex-row-reverse w-full my-3 px-3 overflow-y-hidden'>
                 <button className='bg-slate-700 hover:bg-slate-950 text-white font-bold py-2 px-4 rounded'>Login</button>
               </div>
               <ToastContainer />
